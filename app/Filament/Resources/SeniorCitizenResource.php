701d76126063a953aa1eb9b08cb81e45d6558252
yortzzz <121662445+yortzzz@users.noreply.github.com>
2024-12-06 21:51:00 +0800

    Update SeniorCitizenResource.php

--- app/Filament/Resources/SeniorCitizenResource.php
+++ app/Filament/Resources/SeniorCitizenResource.php
@@ -34,157 +34,151 @@ class SeniorCitizenResource extends Resource
     {
         return $form
             ->schema([
-                Forms\Components\Section::make('Personal Information')
-                    ->description('')
-                    ->schema([
-                        Forms\Components\TextInput::make('osca_id')
-                        ->unique(ignoreRecord: true)
-                            ->numeric(),
-                        Forms\Components\TextInput::make('last_name')
-                            ->required()
-                            ->maxLength(255),
-                        Forms\Components\TextInput::make('first_name')
-                            ->required()
-                            ->maxLength(255),
-                        Forms\Components\TextInput::make('middle_name')
-                            ->required()
-                            ->maxLength(255),
-                        Forms\Components\TextInput::make('extension')
-                            ->required()
-                            ->maxLength(255),
-                        Forms\Components\DatePicker::make('birthday')
-                            ->required()
-                            ->native(false)
-                            ->reactive() // To listen for changes
-                            ->afterStateUpdated(function ($set, $state) {
-                                // Automatically compute the age based on the selected birthday
-                                $age = Carbon::parse($state)->age;  // Compute the age using Carbon
-                                $set('age', $age);  // Set the computed age in the 'age' field
-                            }),
-                        Forms\Components\TextInput::make('age')
+                Forms\Components\Group::make()->schema([
+                    Forms\Components\Section::make('Personal Information')
+                        ->description('')
+                        ->schema([
+                            Forms\Components\TextInput::make('osca_id')
+                                ->unique(ignoreRecord: true)
+                                ->numeric(),
+                            Forms\Components\TextInput::make('last_name')
+                                ->required()
+                                ->maxLength(255),
+                            Forms\Components\TextInput::make('first_name')
+                                ->required()
+                                ->maxLength(255),
+                            Forms\Components\TextInput::make('middle_name')
+                                ->required()
+                                ->maxLength(255),
+                            Forms\Components\TextInput::make('extension')
+                                ->required()
+                                ->maxLength(255),
+                            Forms\Components\DatePicker::make('birthday')
+                                ->required()
+                                ->native(false)
+                                ->reactive() // To listen for changes
+                                ->afterStateUpdated(function ($set, $state) {
+                                    // Automatically compute the age based on the selected birthday
+                                    $age = Carbon::parse($state)->age;  // Compute the age using Carbon
+                                    $set('age', $age);  // Set the computed age in the 'age' field
+                                }),
+                            Forms\Components\TextInput::make('age')
 
-                            ->required(),
-                        Forms\Components\Select::make('gender')
-                            ->options([
-                                'M' => 'Male',
-                                'F' => 'Female'
-                            ])
-                            ->required(),
-                        Forms\Components\Select::make('civil_status')
-                            ->options([
-                                'single' => 'Single',
-                                'married' => 'Married',
-                                'devorced' => 'Devorced',
-                                'legally separated' => 'Legally Separated',
-                                'widowed' => 'Widowed'
-                            ])
-                            ->required(),
-                        Forms\Components\Select::make('religion')
-                            ->options([
-                                'roman catholic' => 'Roman Catholic',
-                                'islam' => 'Islam',
-                                'christian' => 'Christian',
-                                'others' => 'Other',
-                            ])
-                            ->required(),
-                        Forms\Components\TextInput::make('birth_place')
-                            ->required()
-                            ->maxLength(255)
-                            ->columnSpan(2),
-                    ])->columns(3)->compact(),
-
-
-                Forms\Components\Section::make('Address')
-                    ->description('')
-                    ->schema([
-                        Forms\Components\Select::make('city_id')
-                            ->relationship('city', 'name')
-                            ->searchable()
-                            ->preload()
-                            ->createOptionForm([
-                                Forms\Components\TextInput::make('name')
-                                    ->required()
-                                    ->maxLength(255),
-                            ])
-
-                            ->required(),
-                        Forms\Components\Select::make('barangay_id')
-                            ->relationship('barangay', 'name')
-                            ->searchable()
-                            ->preload()
-                            ->createOptionForm([
-                                Forms\Components\TextInput::make('name')
-                                    ->required()
-                                    ->maxLength(255),
-                            ])
-
-
-                            ->required(),
-                        Forms\Components\Select::make('purok_id')
-                            ->relationship('purok', 'name')
-                            ->searchable()
-                            ->preload()
-                            ->createOptionForm([
-                                Forms\Components\TextInput::make('name')
-                                    ->required()
-                                    ->maxLength(255),
-                            ])
-
-                            ->required(),
-                    ])->columns(3)->compact(),
+                                ->required(),
+                            Forms\Components\Select::make('gender')
+                                ->options([
+                                    'M' => 'Male',
+                                    'F' => 'Female'
+                                ])
+                                ->required(),
+                            Forms\Components\Select::make('civil_status')
+                                ->options([
+                                    'single' => 'Single',
+                                    'married' => 'Married',
+                                    'devorced' => 'Devorced',
+                                    'legally separated' => 'Legally Separated',
+                                    'widowed' => 'Widowed'
+                                ])
+                                ->required(),
+                            Forms\Components\Select::make('religion')
+                                ->options([
+                                    'roman catholic' => 'Roman Catholic',
+                                    'islam' => 'Islam',
+                                    'christian' => 'Christian',
+                                    'others' => 'Other',
+                                ])
+                                ->required(),
+                            Forms\Components\TextInput::make('birth_place')
+                                ->required()
+                                ->maxLength(255)
+                                ->columnSpan(2),
+                        ])->columns(3),
+                    Forms\Components\Section::make('Other Information')
+                        ->description('')
+                        ->schema([
+                            Forms\Components\Select::make('educational_attainment')
+                                ->options([
+                                    'elementary graduate' => 'Elementary Graduate',
+                                    'high school graduate' => 'High School Graduate',
+                                    'bachelor degree' => 'Bachelor Degree',
+                                    'master degree' => 'Master Degree',
+                                    'doctorate degree' => 'Doctorate Degree',
+                                ])
+                                ->required(),
+                            Forms\Components\TextInput::make('gsis_id')
+                                ->required()
+                                ->maxLength(255),
+                            Forms\Components\TextInput::make('philhealth_id')
+                                ->required()
+                                ->maxLength(255),
+                            Forms\Components\TextInput::make('illness')
+                                ->required()
+                                ->maxLength(255),
+                            Forms\Components\TextInput::make('disability')
+                                ->required()
+                                ->maxLength(255),
 
 
+                        ])->columns(3),
+                ])->columnSpan(2),
+                Forms\Components\Group::make()->schema([
+                    Forms\Components\Section::make('Address')
+                        ->description('')
+                        ->schema([
+                            Forms\Components\Select::make('city_id')
+                                ->relationship('city', 'name')
+                                ->searchable()
+                                ->preload()
+                                ->createOptionForm([
+                                    Forms\Components\TextInput::make('name')
+                                        ->required()
+                                        ->maxLength(255),
+                                ])
 
-                Forms\Components\Section::make('Other Information')
-                    ->description('')
-                    ->schema([
-                        Forms\Components\TextInput::make('gsis_id')
-                            ->required()
-                            ->maxLength(255),
-                        Forms\Components\TextInput::make('philhealth_id')
-                            ->required()
-                            ->maxLength(255),
-                        Forms\Components\TextInput::make('illness')
-                            ->required()
-                            ->maxLength(255),
-                        Forms\Components\TextInput::make('disability')
-                            ->required()
-                            ->maxLength(255),
-                        Forms\Components\Select::make('educational_attainment')
-                            ->options([
-                                'elementary graduate' => 'Elementary Graduate',
-                                'high school graduate' => 'High School Graduate',
-                                'bachelor degree' => 'Bachelor Degree',
-                                'master degree' => 'Master Degree',
-                                'doctorate degree' => 'Doctorate Degree',
-                            ])
-                            ->required()
-                            ->columnSpan(2),
+                                ->required(),
+                            Forms\Components\Select::make('barangay_id')
+                                ->relationship('barangay', 'name')
+                                ->searchable()
+                                ->preload()
+                                ->createOptionForm([
+                                    Forms\Components\TextInput::make('name')
+                                        ->required()
+                                        ->maxLength(255),
+                                ])
 
-                    ])->columns(2)->compact(),
 
+                                ->required(),
+                            Forms\Components\Select::make('purok_id')
+                                ->relationship('purok', 'name')
+                                ->searchable()
+                                ->preload()
+                                ->createOptionForm([
+                                    Forms\Components\TextInput::make('name')
+                                        ->required()
+                                        ->maxLength(255),
+                                ])
+
+                                ->required(),
+                        ]),
                     Forms\Components\Section::make('Status')
-                    ->description('')
-                    ->schema([
-                        Forms\Components\Toggle::make('is_active')
-                            ->label('Active')
-                            ->default(true)
-                            ->required()
-                            ->reactive()
-                            ->afterStateUpdated(function (callable $set, $state) {
-                                $set('registry_number_disabled', $state); // Enable or disable registry_number based on Toggle state
-                            }),
-                        Forms\Components\TextInput::make('registry_number')
-                            ->disabled(fn ($get) => $get('registry_number_disabled') ?? true) // Check the state to enable/disable
-                            ->required(),
-                    ]),
-                
-                
-                Forms\Components\TextInput::make('full_name')
-                    ->maxLength(255)
-                    ->default(null)
-                    ->hidden(),
-            ]);
+                        ->description('')
+                        ->schema([
+                            Forms\Components\Toggle::make('is_active')
+                                ->label('Active')
+                                ->default(true)
+                                ->required()
+                                ->reactive()
+                                ->afterStateUpdated(function (callable $set, $state) {
+                                    $set('registry_number_hidden', $state); // Hide registry_number when active
+                                }),
+                            Forms\Components\TextInput::make('registry_number')
+                                ->hidden(fn($get) => $get('registry_number_hidden') ?? true) // Hide based on Toggle state
+                                ->required(fn($get) => !($get('registry_number_hidden') ?? true)), // Only required when visible
+                        ]),
+
+                ])->columnSpan(1),
+
+            ])->columns(3);
     }
     public static function infolist(Infolist $infolist): Infolist
     {
@@ -224,7 +218,7 @@ class SeniorCitizenResource extends Resource
     {
         return $table
             ->columns([
-                
+
                 Tables\Columns\TextColumn::make('full_name')
                     ->searchable(),
                 Tables\Columns\TextColumn::make('last_name')
@@ -264,7 +258,7 @@ class SeniorCitizenResource extends Resource
                     ->numeric()
                     ->sortable(),
                 Tables\Columns\TextColumn::make('city.name')
-                ->toggleable(isToggledHiddenByDefault: true)
+                    ->toggleable(isToggledHiddenByDefault: true)
 
                     ->numeric()
                     ->sortable(),
@@ -288,11 +282,11 @@ class SeniorCitizenResource extends Resource
                 Tables\Columns\TextColumn::make('is_active')
                     ->label('Status')
                     ->badge()
-                    ->color(fn (string $state): string => match ($state) {
+                    ->color(fn(string $state): string => match ($state) {
                         '1' => 'primary',
                         '0' => 'danger'
                     })
-                    ->formatStateUsing(fn(string $state):string => match ($state){
+                    ->formatStateUsing(fn(string $state): string => match ($state) {
                         '1' => 'Active',
                         '0' => 'Deceased',
                     }),
@@ -311,11 +305,11 @@ class SeniorCitizenResource extends Resource
                 //
             ])
             ->actions([
-                    Tables\Actions\ActionGroup::make([
-                        Tables\Actions\ViewAction::make('view'),
-                        Tables\Actions\EditAction::make('edit')->slideOver(),
-                        Tables\Actions\DeleteAction::make('delete'),
-                    ])
+                Tables\Actions\ActionGroup::make([
+                    Tables\Actions\ViewAction::make('view'),
+                    Tables\Actions\EditAction::make('edit')->slideOver(),
+                    Tables\Actions\DeleteAction::make('delete'),
+                ])
             ])
             ->bulkActions([
                 Tables\Actions\BulkActionGroup::make([
@@ -340,6 +334,4 @@ class SeniorCitizenResource extends Resource
             // 'edit' => Pages\EditSeniorCitizen::route('/{record}/edit'),
         ];
     }
-    
-
 }
